1a2898602ceeed5c9804432fc7245218893f6ef9
hsan256 <hassan.trabelsi1999@gmail.com>
2024-12-30 18:27:20 +0100

    refactor(agent): enhance function calling by creating a Tool Runner

--- src/memory.ts
+++ src/memory.ts
@@ -1,32 +1,35 @@
 import { JSONFilePreset } from 'lowdb/node'
-import { v4 as uuidv4 } from 'uuid'
 import type { AIMessage } from '../types'
+import { v4 as uuidv4 } from 'uuid'
 
 export type MessageWithMetadata = AIMessage & {
   id: string
   createdAt: string
 }
 
-export const addMetadata = (message: AIMessage): MessageWithMetadata => ({
-  ...message,
-  id: uuidv4(),
-  createdAt: new Date().toISOString(),
-})
-
-export const removeMetadata = (message: MessageWithMetadata): AIMessage => {
-  const { id, createdAt, ...messageWithoutMetadata } = message
-  return messageWithoutMetadata
-}
-
 type Data = {
   messages: MessageWithMetadata[]
 }
 
-const defaultData: Data = { messages: [] }
+export const addMetadata = (message: AIMessage) => {
+  return {
+    ...message,
+    id: uuidv4(),
+    createdAt: new Date().toISOString(),
+  }
+}
+
+export const removeMetadata = (message: MessageWithMetadata) => {
+  const { id, createdAt, ...rest } = message
+  return rest
+}
+
+const defaultData: Data = {
+  messages: [],
+}
 
 export const getDb = async () => {
   const db = await JSONFilePreset<Data>('db.json', defaultData)
-
   return db
 }
 
@@ -39,4 +42,17 @@ export const addMessages = async (messages: AIMessage[]) => {
 export const getMessages = async () => {
   const db = await getDb()
   return db.data.messages.map(removeMetadata)
+}
+
+export const saveToolResponse = async (
+  toolCallId: string,
+  toolResponse: string
+) => {
+  return addMessages([
+    {
+      role: 'tool',
+      content: toolResponse,
+      tool_call_id: toolCallId,
+    },
+  ])
 }
