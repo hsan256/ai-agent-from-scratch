651378ab88f440e29b71d74e46d38756e2c4e07e
hsan256 <hassan.trabelsi1999@gmail.com>
2025-01-02 11:35:58 +0100

    feat(evals): add dad joke evaluation experiment and bring back run script

---
+++ evals/experiments/dadJoke.eval.ts
@@ -0,0 +1,31 @@
+import 'dotenv/config'
+
+import { runLLM } from '../../src/llm'
+import { dadJokeToolDefinition } from '../../src/tools/dadJoke'
+import { runEval } from '../evalTools'
+import { ToolCallMatch } from '../scorers'
+
+const createToolCallMessage = (toolName: string) => ({
+  role: 'assistant',
+  tool_calls: [
+    {
+      type: 'function',
+      function: { name: toolName },
+    },
+  ],
+})
+
+runEval('dadJoke', {
+  task: (input) =>
+    runLLM({
+      messages: [{ role: 'user', content: input }],
+      tools: [dadJokeToolDefinition],
+    }),
+  data: [
+    {
+      input: 'tell me a dad joke',
+      expected: createToolCallMessage(dadJokeToolDefinition.name),
+    },
+  ],
+  scorers: [ToolCallMatch],
+})
--- evals/run.ts
+++ evals/run.ts
@@ -1,28 +1,26 @@
 import 'dotenv/config'
 import { readdir } from 'fs/promises'
 import { dirname, join } from 'path'
-import { fileURLToPath, pathToFileURL } from 'url'
+import { fileURLToPath } from 'url'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
 const main = async () => {
-  const evalName = process.argv[2] // Retrieve the argument passed
+  const evalName = process.argv[2]
   const experimentsDir = join(__dirname, 'experiments')
 
   try {
     if (evalName) {
       const evalPath = join(experimentsDir, `${evalName}.eval.ts`)
-      const evalFileUrl = pathToFileURL(evalPath).href // Convert to file:// URL
-      await import(evalFileUrl)
+      await import(evalPath)
     } else {
       const files = await readdir(experimentsDir)
       const evalFiles = files.filter((file) => file.endsWith('.eval.ts'))
 
       for (const evalFile of evalFiles) {
         const evalPath = join(experimentsDir, evalFile)
-        const evalFileUrl = pathToFileURL(evalPath).href // Convert to file:// URL
-        await import(evalFileUrl)
+        await import(evalPath)
       }
     }
   } catch (error) {
